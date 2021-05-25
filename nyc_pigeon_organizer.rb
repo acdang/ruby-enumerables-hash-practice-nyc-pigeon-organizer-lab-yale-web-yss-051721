4c04bdb632fabb0cd8601b548dc14adb745abd2d
ambitious-caller-8426 <anhcdang@gmail.com>
2021-05-25 02:59:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,3 @@
-require 'pry'
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
 def nyc_pigeon_organizer(data)
   data.each_with_object({}) do |(key, value_hash), final_array|
     value_hash.each do |inner_key, value_array|
@@ -33,5 +13,3 @@ def nyc_pigeon_organizer(data)
     end
   end
 end
-
-nyc_pigeon_organizer(pigeon_data)
